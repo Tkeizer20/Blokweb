2fea07c7e11cf7e998fa6f8396499c82a0aa33fc
Tkeizer20 <144066778+Tkeizer20@users.noreply.github.com>
2023-09-22 14:45:58 +0200

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,2 +1,14 @@
 // JavaScript Document
 console.log("hi");
+
+const hamburger = document.querySelector('.hamburger')
+const navMenu = document.querySelector('.nav_menu')
+
+hamburger.addEventListener ("click", () => {
+    hamburger.classList.toggle("active")
+    navMenu.classList.toggle("active")
+
+
+    console.log(hamburger)
+    console.log (navMenu)
+})
