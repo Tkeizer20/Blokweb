643ec2b032d2ddc418195b8ba97f5e3468b930be
Tkeizer20 <144066778+Tkeizer20@users.noreply.github.com>
2023-11-09 11:50:33 +0100

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,16 +1,13 @@
 // JavaScript Document
+
 console.log("hi");
 
-const hamburger = document.querySelector('.hamburger')
-const navMenu = document.querySelector('.nav_menu')
+const hamburger = document.querySelector('.hamburger2')
+const navMenu = document.querySelector('.hidden_nav ul')
 
 hamburger.addEventListener ("click", () => {
-    hamburger.classList.toggle("active")
+    hamburger.classList.toggle("cross");
     navMenu.classList.toggle("active")
-
-
-    console.log(hamburger)
-    console.log (navMenu)
 })
 
 
@@ -20,15 +17,24 @@ hamburger.addEventListener ("click", () => {
 
 
 function openPopup() {
+ 
     var popup = document.getElementById("popup");
     popup.classList.add("open-popup");
+   
 }
-
 
 function closePopup() {
     var popup = document.getElementById("popup");
     popup.classList.remove("open-popup");
 }
+
+
+
+
+
+
+
+
 
 const hiddenNav= document.querySelector('hidden_nav')
 
@@ -38,19 +44,21 @@ window.addEventListener("scroll", function () {
     } else {
       document.querySelector(".hidden_nav").classList.remove("active");
     }
+
+
   });
 
 //Bron:
   
 
 
-const hamburger2 = document.querySelector('.hamburger2')
+// const hamburger2 = document.querySelector('.hamburger2')
 
-hamburger.addEventListener ("click", () => {
-    hamburger.classList.toggle("active")
-    navMenu.classList.toggle("active")
+// hamburger.addEventListener ("click", () => {
+//     hamburger.classList.toggle("active")
+//     navMenu.classList.toggle("active")
 
 
-    console.log(hamburger)
-    console.log (navMenu)
-})
+//     console.log(hamburger)
+//     console.log (navMenu)
+// })
